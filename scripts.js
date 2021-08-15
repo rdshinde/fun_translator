5e78d12e4696f7586fb3fc49b3e037b81943d014
Rishikesh Shinde <rdshinde2000@gmail.com>
2021-08-15 13:21:49 +0530

    till about section

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-console.log("Hello World!")
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show-links");
+});
